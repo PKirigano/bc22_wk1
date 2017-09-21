1f2092500de4ce60444aefe38cd2ba65d07f6abc
Paul Kirigano <Paul Kirigano>
2017-09-21 18:09:52 +0300

    More files, a class and python routing added

--- day2/app/shoppingList.py
+++
@@ -1,6 +0,0 @@
-class shoppingList(object):
-    def __init__(self, shoppingListName, shoppingListDescription):
-        self.shoppingListName = shoppingListName
-        self.shoppingListDescription = shoppingListDescription
-    
-    def addItem
